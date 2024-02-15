2bfea05c29ecdc77b100890a62eb45de0a9b9381
Quistaaan <70280952+Quistaaan@users.noreply.github.com>
2024-02-15 13:34:49 -0700

    added extra print statements to show variation in the results

--- SpinFile.py
+++ SpinFile.py
@@ -1,4 +1,5 @@
 #Written By Austin Mills
+#https://github.com/Quistaaan/A4
 
 import Spinner
 
@@ -6,5 +7,7 @@ def main():
     spinner = Spinner.Spinner('essay.txt', 'synonyms-simplified.txt')
     print('original: ' + spinner.text + '\n')
     print('changed: ' + spinner.change_words())
+    print(spinner.change_words())
+    print(spinner.change_words())
 if __name__ == "__main__":
     main()
