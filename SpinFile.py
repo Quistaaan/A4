14f621991ac4bd1c6c12f7d9fbd4bb48dc9d4737
Quistaaan <70280952+Quistaaan@users.noreply.github.com>
2024-02-15 09:50:35 -0700

    added clean up lines method and synonym dictionary method

--- SpinFile.py
+++ SpinFile.py
@@ -1,7 +1,14 @@
-
+import Spinner
 def read_file(text_file):
-    text_file.read().
+    text = text_file.read()
+    text = text.strip()
+    text = text.lower()
+    return text
 def main():
+    original_text = read_file(open ('essay.txt'))
+    print('Original: ' + original_text)
+    spinner = Spinner.Spinner(original_text, 'synonyms-simplified.txt')
+    print(spinner.clean_extra_lines())
 
 
 
--- Spinner.py
+++ Spinner.py
@@ -1,4 +1,12 @@
-
+import SpinFile
 
 class Spinner:
-    def __init__(self):
+    def __init__(self, text_file, synonyms):
+        self.text = text_file
+        self.synonyms = synonyms
+
+    def clean_extra_lines(self):
+        self.text = self.text.strip('\n')
+        return self.text
+
+    def create_synonym_dict(self):
